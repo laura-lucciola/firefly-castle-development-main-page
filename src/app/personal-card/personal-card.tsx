3d635daf48bc3918cb9c403f53503a13660f03b2
Laura Lucciola <laura@firefly-castle.dev>
2024-05-17 15:21:24 +0100

    Adds folders

---
+++ src/app/personal-card/personal-card.tsx
@@ -0,0 +1,16 @@
+import Card from 'react-bootstrap/Card';
+
+export function PersonalCard() {
+    return (<Card id="who-am-i-card" className="who-am-i-card">
+        <Card.Header id="who-am-i-header">Who am I?</Card.Header>
+        <Card.Subtitle id="who-am-i-subtitle">Laura Lucciola</Card.Subtitle>
+        <Card.Body>
+            <Card.Text id="who-am-i-text">
+                Driven Senior Software Engineer on a continuous journey for growth. An adaptable, hard-working,
+                organized, and reliable software engineer eager to learn from other cultures and people from
+                different backgrounds. I am passionate about using technology to improve people's lives and
+                enhance my skills in the process.
+            </Card.Text>
+        </Card.Body>
+    </Card>);
+}
