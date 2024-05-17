a728266fb0c607c3460092b34b145fbb534d4204
Laura Lucciola <laura@firefly-castle.dev>
2024-05-17 16:54:05 +0100

    Starts adding variables

--- src/app/navigation-bar/navigation-bar.tsx
+++ src/app/navigation-bar/navigation-bar.tsx
@@ -1,17 +1,19 @@
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import logo from '../../assets/company-logo.svg';
 
 function NavigationBar() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar bg="primary" data-bs-theme="light">
             <Container>
-                <Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/contact">Contact</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
+                <Navbar.Brand href="/">
+                    <img src={logo} width="40" height="40" className="d-inline-block align-top" alt="Company logo" />
+                </Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/contact">Contact</Nav.Link>
+                </Nav>
             </Container>
         </Navbar>
     );
