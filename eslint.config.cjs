f15ee302a243a5c3f51d3b3face4713c867ca480
laura <laura@firefly-castle.dev>
2024-08-05 16:09:48 -0300

    Redo the lint and prettier config

---
+++ eslint.config.cjs
@@ -0,0 +1,48 @@
+const prettier = require('eslint-plugin-prettier');
+const react = require('eslint-plugin-react');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const globals = require('globals');
+const tsParser = require('@typescript-eslint/parser');
+const js = require('@eslint/js');
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+module.exports = [
+    ...compat.extends(
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+        'plugin:@typescript-eslint/recommended'
+    ),
+    {
+        files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+        ignores: ['node_modules', 'build', '!**/eslint.config.js', '**/*.config.cjs'],
+        plugins: {
+            prettier,
+            react,
+            '@typescript-eslint': typescriptEslint,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 'latest',
+            sourceType: 'commonjs',
+
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+    },
+];
