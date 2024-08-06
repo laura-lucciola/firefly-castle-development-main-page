a123221fcb7dcd05a9eafcfce2ff23303efef6ec
laura <laura@firefly-castle.dev>
2024-08-06 15:24:09 -0300

    Adds spinner and alert style

--- src/app/contact-page/contact-page.tsx
+++ src/app/contact-page/contact-page.tsx
@@ -5,18 +5,19 @@ import ContactForm from '../contact-form/contact-form';
 import { useState } from 'react';
 import { ContactFormValues } from '../contact-form/contact-form-values';
 import { sendContactEmail } from './send-contact-email';
+import './contact-page.scss';
 
 function ContactPage() {
     const { t } = useTranslation();
     const { theme } = useTheme();
     const [loading, setLoading] = useState(false);
-    const [showAlert, setShowAlert] = useState(true);
+    const [showAlert, setShowAlert] = useState(false);
     const [alertVariant, setAlertVariant] = useState('success');
 
     const onFormSubmit = (ev: ContactFormValues) => {
         setLoading(true);
         sendContactEmail(ev).then(
-            _ => {
+            () => {
                 setLoading(false);
                 setShowAlert(true);
                 setAlertVariant('success');
@@ -32,13 +33,15 @@ function ContactPage() {
     };
 
     return (
-        <div>
+        <div id="contact-page" className="contact-page">
             <Card id="contact-card" className={`${theme} contact-card`}>
                 <Card.Header>{t('contactCard.header')}</Card.Header>
 
                 <Card.Body>
                     {loading ? (
-                        <Spinner animation="border" role="status"></Spinner>
+                        <Spinner animation="border" role="status" variant="success">
+                            <span className="visually-hidden">{t('common.loading')}</span>
+                        </Spinner>
                     ) : (
                         <ContactForm onSubmit={values => onFormSubmit(values)}></ContactForm>
                     )}
@@ -49,7 +52,7 @@ function ContactPage() {
                 </Card.Footer>
             </Card>
             {showAlert && (
-                <Alert variant={alertVariant} dismissible>
+                <Alert id="contact-email-alert" className="contact-email-alert" variant={alertVariant} dismissible>
                     {t(`contactCard.emailAlert.${alertVariant}`)}
                 </Alert>
             )}
