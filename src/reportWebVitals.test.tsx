d43d9cc82ea68d4d37887ac5d7faff98fd087a78
Laura Lucciola <laura@firefly-castle.dev>
2026-05-10 17:53:56 +0100

    jest test config

---
+++ src/reportWebVitals.test.tsx
@@ -0,0 +1,20 @@
+import reportWebVitals from './reportWebVitals';
+
+describe('reportWebVitals', () => {
+    it('should not error if callback is provided', () => {
+        const mockCallback = jest.fn();
+        expect(() => reportWebVitals(mockCallback)).not.toThrow();
+    });
+
+    it('should not error if callback is undefined', () => {
+        expect(() => reportWebVitals(undefined)).not.toThrow();
+    });
+
+    it('should not error if callback is not a function', () => {
+        expect(() => reportWebVitals('not a function' as any)).not.toThrow();
+    });
+
+    it('should be a function that accepts a callback', () => {
+        expect(typeof reportWebVitals).toBe('function');
+    });
+});
